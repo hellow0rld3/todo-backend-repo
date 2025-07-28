2b1a23d94f831d0d90309bc86ca394be5d1e3c07
Cezary Szczepanek <cszczepanek43@gmail.com>
2025-07-28 18:04:17 +0200

    Added CORS

---
+++ src/main/java/com/example/todobackend/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.todobackend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://jazzy-fox-d38cb9.netlify.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
